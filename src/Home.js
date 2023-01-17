0bc722dad29f766339fb12869a5e37a0cda7f2c4
“mmorrison92” <madison@appcues.com>
2023-01-17 11:21:48 -0500

    removing dropdown menu from home page

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,14 @@ const Home = () => {
       <h1>Welcome to this feature-rich test application</h1>
       <h2>We're glad you're here</h2>
       <img src={squirrel} alt="a cute little squirrel peering at you from the tall grass"></img>
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      {/* <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown button
-  </button>
+  </button> 
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
     <li><a class="dropdown-item" href="https://www.google.com/">Google</a></li>
     <li><a class="dropdown-item" href="https://studio.appcues.com/">Studio in Production</a></li>
     <li><a class="dropdown-item" href="https://studio-staging.appcues.com/">Studio in Staging</a></li>
-  </ul>
+  </ul> */}
     </div>
   );
 };
